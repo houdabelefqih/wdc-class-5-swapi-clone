61f9cf4d0d89ab5d21375db237bf106230270dbd
Ivan Zugnoni <zugnoni.ivan@gmail.com>
2018-12-19 15:51:14 -0300

    Add training solution

--- swapi/training/views.py
+++ swapi/training/views.py
@@ -10,7 +10,7 @@ def text_response(request):
     Return a HttpResponse with a simple text message.
     Check that the default content type of the response must be "text/html".
     """
-    pass
+    return HttpResponse('Hello World! This is not a JSON')
 
 
 def looks_like_json_response(request):
@@ -18,7 +18,7 @@ def looks_like_json_response(request):
     Return a HttpResponse with a text message containing something that looks
     like a JSON document, but it's just "text/html".
     """
-    pass
+    return HttpResponse('{"name": "Luke Skywalker"}')
 
 
 def simple_json_response(request):
@@ -26,14 +26,17 @@ def simple_json_response(request):
     Return an actual JSON response by setting the `content_type` of the HttpResponse
     object manually.
     """
-    pass
+    return HttpResponse(
+        '{"name": "Luke Skywalker"}',
+        content_type='application/json')
 
 
 def json_response(request):
     """
     Return the same JSON document, but now using a JsonResponse instead.
     """
-    pass
+    new_dict = {"name": "Luke Skywalker"}
+    return JsonResponse(new_dict)
 
 
 def json_list_response(request):
@@ -44,14 +47,19 @@ def json_list_response(request):
     the JsonResponse object it order to avoid built-in validation.
     https://docs.djangoproject.com/en/2.0/ref/request-response/#jsonresponse-objects
     """
-    pass
+    new_list = [
+        {"name": "Luke Skywalker"},
+        {"name": "R2-D2"},
+    ]
+    return JsonResponse(new_list, safe=False)
 
 
 def json_error_response(request):
     """
     Return a JsonResponse with an error message and 400 (Bad Request) status code.
     """
-    pass
+    new_dict = {"success": False, "error": "Something went wrong"}
+    return JsonResponse(new_dict, status=400)
 
 
 @csrf_exempt
@@ -61,7 +69,10 @@ def only_post_request(request):
     everything is OK, and the status code `200`. If it's a different request
     method, return a `400` response with an error message.
     """
-    pass
+    if request.method != 'POST':
+        return JsonResponse(
+            {"success": False, "msg": "We only allow POST requests"}, status=400)
+    return JsonResponse({"success": True, "msg": "Yay! You did it."})
 
 
 @csrf_exempt
@@ -70,14 +81,30 @@ def post_payload(request):
     Write a view that only accepts POST requests, and processes the JSON
     payload available in `request.body` attribute.
     """
-    pass
+    if request.method != 'POST':
+        return JsonResponse(
+            {"success": False, "msg": "We only support POST requests"}, status=400)
+
+    try:
+        payload = json.loads(request.body)
+    except ValueError:
+        return JsonResponse(
+            {"success": False, "msg": "Provide a valid JSON payload"}, status=400)
+
+    if not payload:
+        msg = "We got no payload :-("
+    else:
+        msg = "Yay! We got your payload: {}".format(dict(payload))
+    return JsonResponse({"success": True, "msg": msg})
 
 
 def custom_headers(request):
     """
     Return a JsonResponse and add a custom header to it.
     """
-    pass
+    response = JsonResponse({"success": True})
+    response['X-RMOTR-IS-AWESOME'] = True
+    return response
 
 
 def url_int_argument(request, first_arg):
@@ -85,7 +112,8 @@ def url_int_argument(request, first_arg):
     Write a view that receives one integer parameter in the URL, and displays it
     in the response text.
     """
-    pass
+    return JsonResponse(
+        {"success": True, "msg": "We received this argument: {}".format(first_arg)})
 
 
 def url_str_argument(request, first_arg):
@@ -93,7 +121,8 @@ def url_str_argument(request, first_arg):
     Write a view that receives one string parameter in the URL, and displays it
     in the response text.
     """
-    pass
+    return JsonResponse(
+        {"success": True, "msg": "We received this argument: {}".format(first_arg)})
 
 
 def url_multi_arguments(request, first_arg, second_arg):
@@ -101,7 +130,8 @@ def url_multi_arguments(request, first_arg, second_arg):
     Write a view that receives two parameters in the URL, and display them
     in the response text.
     """
-    pass
+    return JsonResponse(
+        {"success": True, "msg": "We received this two arguments: {}, {}".format(first_arg, second_arg)})
 
 
 def get_params(request):
@@ -109,4 +139,9 @@ def get_params(request):
     Write a view that receives GET arguments and display them in the
     response text.
     """
-    pass
+    arguments = request.GET
+    if not arguments:
+        msg = "We got no arguments :-("
+    else:
+        msg = "Yay! We got your arguments: {}".format(dict(arguments))
+    return JsonResponse({"success": True, "msg": msg})

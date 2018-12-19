407505a4e3b0e36fe47c38ffecf7f0b953f35c43
Ivan Zugnoni <zugnoni.ivan@gmail.com>
2018-12-19 16:20:30 -0300

    Implement REST views with functions. Delete Planet model

--- swapi/api/serializers.py
+++ swapi/api/serializers.py
@@ -3,7 +3,6 @@
 def serialize_people_as_json(people):
     return {
         'name': people.name,
-        'homeworld': 'http://localhost:8000/planets/{}/'.format(people.homeworld.id),
         'height': people.height,
         'mass': people.mass,
         'hair_color': people.hair_color,
--- swapi/api/views.py
+++ swapi/api/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-from api.models import Planet, People
+from api.models import People
 from api.fixtures import SINGLE_PEOPLE_OBJECT, PEOPLE_OBJECTS
 from api.serializers import serialize_people_as_json
 
@@ -21,42 +21,105 @@ def list_people(request):
 def people_list_view(request):
     """
     People `list` actions:
-
     Based on the request method, perform the following actions:
-
         * GET: Return the list of all `People` objects in the database.
-
         * POST: Create a new `People` object using the submitted JSON payload.
-
     Make sure you add at least these validations:
-
         * If the view receives another HTTP method out of the ones listed
           above, return a `400` response.
-
         * If submited payload is nos JSON valid, return a `400` response.
     """
-    pass
+    if request.body:
+        try:
+            payload = json.loads(request.body)
+        except ValueError:
+            return JsonResponse(
+                {"success": False, "msg": "Provide a valid JSON payload"},
+                status=400)
+
+    status = 200
+    if request.method == 'GET':
+        # GET /people
+        qs = People.objects.all()
+        data = [serialize_people_as_json(people) for people in qs]
+    elif request.method == 'POST':
+        # POST /people
+        try:
+            people = People.objects.create(
+                name=payload['name'],
+                height=payload['height'],
+                mass=payload['mass'],
+                hair_color=payload['hair_color'])
+        except (ValueError, KeyError):
+            return JsonResponse(
+                {"success": False, "msg": "Provided payload is not valid"},
+                status=400)
+        data = serialize_people_as_json(people)
+        status = 201
+    else:
+        data = {"success": False, "msg": "Invalid HTTP method"}
+        status=400
+
+    return JsonResponse(data, status=status, safe=False)
 
 
 @csrf_exempt
 def people_detail_view(request, people_id):
     """
     People `detail` actions:
-
     Based on the request method, perform the following actions:
-
         * GET: Returns the `People` object with given `people_id`.
-
         * PUT/PATCH: Updates the `People` object either partially (PATCH)
           or completely (PUT) using the submitted JSON payload.
-
         * DELETE: Deletes `People` object with given `people_id`.
-
     Make sure you add at least these validations:
-
         * If the view receives another HTTP method out of the ones listed
           above, return a `400` response.
-
         * If submited payload is nos JSON valid, return a `400` response.
     """
-    pass
+    try:
+        people = People.objects.get(id=people_id)
+    except People.DoesNotExist:
+        return JsonResponse(
+            {"success": False, "msg": "Could not find people with id: {}".format(people_id)},
+            status=404)
+
+    if request.body:
+        try:
+            payload = json.loads(request.body)
+        except ValueError:
+            return JsonResponse(
+                {"success": False, "msg": "Provide a valid JSON payload"},
+                status=400)
+
+    status = 200
+    if request.method == 'GET':
+        # GET /people/:id
+        data = serialize_people_as_json(people)
+    elif request.method in ['PUT', 'PATCH']:
+        # PUT/PATCH /people/:id
+        for field in ['name', 'homeworld', 'height', 'mass', 'hair_color']:
+            if not field in payload:
+                if request.method == 'PATCH':
+                    continue
+                return JsonResponse(
+                    {"success": False, "msg": "Missing field in full update"},
+                    status=400)
+
+            try:
+                setattr(people, field, payload[field])
+                people.save()
+            except ValueError:
+                return JsonResponse(
+                    {"success": False, "msg": "Provided payload is not valid"},
+                    status=400)
+        data = serialize_people_as_json(people)
+    elif request.method == 'DELETE':
+        # DELETE /people/:id
+        people.delete()
+        data = {"success": True}
+    else:
+        data = {"success": False, "msg": "Invalid HTTP method"}
+        status=400
+
+    return JsonResponse(data, status=status, safe=False)
